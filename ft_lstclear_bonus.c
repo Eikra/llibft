fa3e1c5bfc10e7f45964ab74356290845d556a01
Ikrame Echaraki <iecharak@e1r13p5.1337.ma>
2022-10-26 15:18:31 +0100

    j

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -6,10 +6,13 @@
 /*   By: iecharak <iecharak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 14:25:39 by iecharak          #+#    #+#             */
-/*   Updated: 2022/10/26 14:28:45 by iecharak         ###   ########.fr       */
+/*   Updated: 2022/10/26 15:16:14 by iecharak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+    
+}
